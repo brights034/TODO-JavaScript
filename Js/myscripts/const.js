d81a1513de26cc2db40878460241985857cc4427
Brightsunny <106729314+B619Ht@users.noreply.github.com>
2023-03-17 10:29:22 +0530

    script

--- Js/myscripts/const.js
+++ Js/myscripts/const.js
@@ -6,5 +6,5 @@
 
 const FCC = "freeCodeCamp"; // Change this line
 let fact = "is cool!"; // Change this line
-fact = "is awesome!";
+var fact = "is awesome!";
 console.log(FCC, fact); // Change this line
