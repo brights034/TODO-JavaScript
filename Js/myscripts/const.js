2d3d267c7a62eb628658d78ebd8ca3ddc69f11b3
Brightsunny <106729314+B619Ht@users.noreply.github.com>
2023-03-17 10:28:14 +0530

    script

--- Js/myscripts/const.js
+++ Js/myscripts/const.js
@@ -1,3 +1,10 @@
-const FAV_PET = "Cats";
-FAV_PET = "Dogs";
-console.log(FAV_PET);
+//test//
+
+// const FAV_PET = "Cats";
+// FAV_PET = "Dogs";
+// console.log(FAV_PET);
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
