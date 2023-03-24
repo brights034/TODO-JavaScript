cde0109c81909850bebdd5a55bac8df0b71c8a21
Brightsunny <106729314+B619Ht@users.noreply.github.com>
2023-03-24 10:42:11 +0530

    test

--- Js/myscripts/const.js
+++ Js/myscripts/const.js
@@ -7,4 +7,4 @@
 const FCC = "freeCodeCamp"; // Change this line
 let fact = "is cool!"; // Change this line
 fact = "is awesome!";
-console.log(FCC, fact); // Change this line
+//console.log(FCC, fact); // Change this line
--- Js/myscripts/lengthandbracketnotation.js
+++ Js/myscripts/lengthandbracketnotation.js
@@ -2,4 +2,4 @@ const lastName = "Lovelace";
 
 // Only change code below this line
 const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
-console.log(secondToLastLetterOfLastName)
+//console.log(secondToLastLetterOfLastName)
