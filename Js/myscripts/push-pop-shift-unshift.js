d0fb126b9971c5ad92410d3ba1fc4389666f4e7c
Brightsunny <106729314+B619Ht@users.noreply.github.com>
2023-03-29 22:23:55 +0530

    test

--- Js/myscripts/push-pop-shift-unshift.js
+++ Js/myscripts/push-pop-shift-unshift.js
@@ -3,10 +3,10 @@
 //*.push() used to push one or more parameters and "pushes" them onto the end of the array.*//
 
 // Setup
-const myArray = [["John", 23], ["cat", 2]];
+const myArray = [["Akhil",[25, 15-09-1997], "a@gmail.com" ], ["Bright", [25, 22-05-1997], "b@gmail.com"],["Sreepadh",[25, 20-11-1997],"s@gmail.com"]];
 
 
-myArray.push(["dog", 3]);
+myArray.push(["vibin"]);
 console.log(myArray);
 
 //*.pop() is used to pop a value off of the end of an array.*//
@@ -15,16 +15,16 @@ const Array = [["John", 23], ["cat", 2]];
 
 
 const removedFromArray = Array.pop();
-console.log(removedFromArray);
-console.log(Array);
+//console.log(removedFromArray);
+//console.log(Array);
 
 //* .shift() removes the first element instead of the last.*//
 // Setup
 const det = [["John", 23], ["dog", 3]];
 
 const removeddet = det.shift();
-console.log(removeddet);
-console.log(det);
+//console.log(removeddet);
+//console.log(det);
 
 //* .unshift() adds the element at the beginning of the array.*//
 // Setup
@@ -33,4 +33,4 @@ details.shift();
 
 // Only change code below this line
 details.unshift(["Paul", 35]);
-console.log(details);
+//console.log(details);
