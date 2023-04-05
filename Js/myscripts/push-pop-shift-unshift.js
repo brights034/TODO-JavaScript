6c6922f487f755eac0dae80938ffb4cfdf473606
Brightsunny <106729314+B619Ht@users.noreply.github.com>
2023-04-05 19:45:18 +0530

    test

--- Js/myscripts/push-pop-shift-unshift.js
+++ Js/myscripts/push-pop-shift-unshift.js
@@ -1,40 +1,31 @@
 
 
-//*.push() used to push one or more parameters and "pushes" them onto the end of the array.*//
+// //*.push() used to push one or more parameters and "pushes" them onto the end of the array.*//
 
-// Setup
-const myArray = [["Akhil",[25, "15-09-1997"], "a@gmail.com" ], ["Bright", [25, "22-05-1997"], "b@gmail.com"],["Sreepadh",[25, "20-11-1997"],"s@gmail.com"]];
+// // Setup
+// const myArray = [["Akhil", [25, "15-09-1997"], "a@gmail.com"], ["Bright", [25, "22-05-1997"], "b@gmail.com"], ["Sreepadh", [25, "20-11-1997"], "s@gmail.com"]];
 
 
 // myArray.push(["vibin"]);
-//  console.log(myArray);
+// console.log(myArray);
 
 
-//*.pop() is used to pop a value off of the end of an array.*//
+// //*.pop() is used to pop a value off of the end of an array.*//
 // const removedFromArray = myArray.pop();
-// // console.log(removedFromArray);
-// // console.log(myArray);
+// console.log(removedFromArray);
+// console.log(myArray);
 
 // //* .shift() removes the first element instead of the last.*//
 // const removefarray = myArray.shift();
-// //console.log(removefarray);
-// //console.log(myArray);
+// console.log(removefarray);
+// console.log(myArray);
 
-// // Setup
-// // const det = [["John", 23], ["dog", 3]];
-
-// //const removeddet = det.shift();
-// //console.log(removeddet);
-// //console.log(det);
 
 // //* .unshift() adds the element at the beginning of the array.*//
 // // Setup
-const myArrays = [["Akhil",[25, "15-09-1997"], "a@gmail.com" ], ["Bright", [25, "22-05-1997"], "b@gmail.com"],["Sreepadh",[25, "20-11-1997"],"s@gmail.com"]];
+const myArrays = [["Akhil", [25, "15-09-1997"], "a@gmail.com"], ["Bright", [25, "22-05-1997"], "b@gmail.com"], ["Sreepadh", [25, "20-11-1997"], "s@gmail.com"]];
 
- myArrays.shift();
-//details.shift();
-
-// // Only change code below this line
-myArrays.unshift(["Akhil",[25, "15-09-1997"], "a@gmail.com" ]);
- console.log(myArrays);
-// console.log(myArray);
+myArrays.shift();
+console.log(myArrays);
+myArrays.unshift(["Akhil", [25, "15-09-1997"], "a@gmail.com"]);
+console.log(myArrays);
