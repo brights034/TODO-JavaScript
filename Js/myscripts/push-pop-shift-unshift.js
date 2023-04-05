a526d38487ad061406ea4d5726bbd296cf1263f7
Brightsunny <106729314+B619Ht@users.noreply.github.com>
2023-04-05 19:38:53 +0530

    test

--- Js/myscripts/push-pop-shift-unshift.js
+++ Js/myscripts/push-pop-shift-unshift.js
@@ -32,9 +32,9 @@ const myArray = [["Akhil",[25, "15-09-1997"], "a@gmail.com" ], ["Bright", [25, "
 const myArrays = [["Akhil",[25, "15-09-1997"], "a@gmail.com" ], ["Bright", [25, "22-05-1997"], "b@gmail.com"],["Sreepadh",[25, "20-11-1997"],"s@gmail.com"]];
 
  const details = myArrays.shift();
-// //details.shift();
+//details.shift();
 
 // // Only change code below this line
-// details.unshift(["Akhil",[25, "15-09-1997"], "a@gmail.com" ]);
+ details.unshift(["Akhil",[25, "15-09-1997"], "a@gmail.com" ]);
  console.log(details);
 // console.log(myArray);
