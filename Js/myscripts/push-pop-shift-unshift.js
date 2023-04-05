4107ff7e1c9feb5bb1c0c660bf9dc6bb24fec8f5
Brightsunny <106729314+B619Ht@users.noreply.github.com>
2023-04-05 19:48:30 +0530

    test

--- Js/myscripts/push-pop-shift-unshift.js
+++ Js/myscripts/push-pop-shift-unshift.js
@@ -1,25 +1,25 @@
 
 
-//*.push() used to push one or more parameters and "pushes" them onto the end of the array.*//
+// //*.push() used to push one or more parameters and "pushes" them onto the end of the array.*//
 
-// Setup
-const myArray = [["Akhil", [25, "15-09-1997"], "a@gmail.com"], ["Bright", [25, "22-05-1997"], "b@gmail.com"], ["Sreepadh", [25, "20-11-1997"], "s@gmail.com"]];
-
-
-myArray.push(["vibin"]);
-console.log(myArray);
+// // Setup
+// const myArray = [["Akhil", [25, "15-09-1997"], "a@gmail.com"], ["Bright", [25, "22-05-1997"], "b@gmail.com"], ["Sreepadh", [25, "20-11-1997"], "s@gmail.com"]];
 
 
-//*.pop() is used to pop a value off of the end of an array.*//
-console.log(myArray);
-const removedFromArray = myArray.pop();
-console.log(removedFromArray);
+// myArray.push(["vibin"]);
+// console.log(myArray);
 
 
-//* .shift() removes the first element instead of the last.*//
-const removefarray = myArray.shift();
-console.log(removefarray);
-console.log(myArray);
+// //*.pop() is used to pop a value off of the end of an array.*//
+// console.log(myArray);
+// const removedFromArray = myArray.pop();
+// console.log(removedFromArray);
+
+
+// //* .shift() removes the first element instead of the last.*//
+// const removefarray = myArray.shift();
+// console.log(removefarray);
+// console.log(myArray);
 
 
 //* .unshift() adds the element at the beginning of the array.*//
